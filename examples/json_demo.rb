2581b59ee1f123cdaf43d110358c4571bf25f8c9
Mike Williams <mdub@dogbiscuit.org>
2010-08-11 20:31:29 +1000

    Create a JSON example (doesn't work yet).

---
+++ examples/json_demo.rb
@@ -0,0 +1,42 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require "rubygems"
+require "representative/json"
+require "ostruct"
+
+books = [
+  OpenStruct.new(
+    :title => "Sailing for old dogs", 
+    :authors => ["Jim Watson"],
+    :published => OpenStruct.new(
+      :by => "Credulous Print",
+      :year => 1994
+    )
+  ),
+  OpenStruct.new(
+    :title => "On the horizon", 
+    :authors => ["Zoe Primpton", "Stan Ford"],
+    :published => OpenStruct.new(
+      :by => "McGraw-Hill",
+      :year => 2005
+    )
+  ),
+  OpenStruct.new(
+    :title => "The Little Blue Book of VHS Programming",
+    :authors => ["Henry Nelson"],
+    :rating => "****"
+  )
+]
+
+json = Representative::Json.new do |r|
+  r.list_of :books, books do
+    r.element :title
+    r.list_of :authors
+    r.element :published do
+      r.element :by
+      r.element :year
+    end
+  end
+end
+
+puts json.to_json
