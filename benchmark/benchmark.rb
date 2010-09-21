26308a076b8c546e24732d0e44595ee2546fd9d0
Mike Williams <mdub@dogbiscuit.org>
2010-09-22 07:37:39 +1000

    Benchmark the various implementations.

---
+++ benchmark/benchmark.rb
@@ -0,0 +1,79 @@
+$: << File.expand_path("../../lib", __FILE__)
+
+require "rubygems"
+
+require "benchmark"
+require "ostruct"
+
+require "representative/json"
+require "representative/nokogiri"
+require "representative/xml"
+
+@books = [
+  OpenStruct.new(
+    :title => "Sailing for old dogs", 
+    :authors => ["Jim Watson"],
+    :published => OpenStruct.new(
+      :by => "Credulous Print",
+      :year => 1994
+    )
+  ),
+  OpenStruct.new(
+    :title => "On the horizon", 
+    :authors => ["Zoe Primpton", "Stan Ford"],
+    :published => OpenStruct.new(
+      :by => "McGraw-Hill",
+      :year => 2005
+    )
+  ),
+  OpenStruct.new(
+    :title => "The Little Blue Book of VHS Programming",
+    :authors => ["Henry Nelson"],
+    :rating => "****"
+  )
+]
+
+def represent_books_using(r)
+
+  r.list_of :books, @books do
+    r.element :title
+    r.list_of :authors
+    r.element :published do
+      r.element :by
+      r.element :year
+    end
+  end
+
+end
+
+iterations = 1000
+
+Benchmark.bmbm do |x|
+
+  x.report("builder") do
+    iterations.times do
+      xml = Builder::XmlMarkup.new(:indent => 2)
+      r = Representative::Xml.new(xml)
+      represent_books_using(r)
+      xml.target!
+    end
+  end
+
+  x.report("nokogiri") do
+    iterations.times do
+      xml = Builder::XmlMarkup.new(:indent => 2)
+      r = Representative::Nokogiri.new
+      represent_books_using(r)
+      r.to_xml
+    end
+  end
+
+  x.report("json") do
+    iterations.times do
+      r = Representative::Json.new
+      represent_books_using(r)
+      r.to_json
+    end
+  end
+
+end
